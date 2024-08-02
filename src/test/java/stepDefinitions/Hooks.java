3aa0d346e99145399697d23502950922deeb7954
Juan <anzolajuan.11@gmail.com>
2024-08-02 10:44:48 -0500

    Add aplicattionProperties and hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,37 @@
 package stepDefinitions;
 
-public class Hooks {
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
+
+public class Hooks {
+    private static ChromeDriver driver;
+
+    @Before
+    public void initBrowser() {
+        try {
+            System.setProperty("webdriver.chrome.driver", "C:\\Driver Chrome/chromedriver.exe");
+            ChromeDriver driver = new ChromeDriver();
+            driver.get();
+            driver.manage().window().maximize();
+        }catch (Exception e){
+            System.out.println("Error:"+e.getMessage());
+        }
+    }
+
+
+    @After
+    public void closeBrowser (){
+        try {
+            driver.close();
+        }catch (Exception e){
+            System.out.println("Error:"+e.getMessage());
+        }
+    }
+
+    public static ChromeDriver getDriver(){
+        return driver;
+    }
 }
